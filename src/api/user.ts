870130d9e2a97aff3539d1ca38a58182564929de
Edouardgj <edouard.grandjean@hotmail.fr>
2022-04-16 21:05:20 +0200

    MAJ User

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,4 +1,5 @@
 import { User } from './types'
+import axios from 'axios'
 
 const base_url = 'http://localhost:3004/users'
 
@@ -40,6 +41,26 @@ function getAllUser(): Array<User> {
         },
     ]
 }
+async function createUser(user: User): Promise<User> {
+    // create a new post
+    // [TODO] remove this return to use a fetch API
+    const { data } = await axios.post(`${base_url}`, user)
+    return data
+}
+async function updateUser(user: User): Promise<User> {
+    // update a existing post
+    // [TODO] remove this return to use a fetch API
+    const { data } = await axios.put(`${base_url}/${user.id}`, user)
+    return data
+}
+
+async function deleteUser(userID: User['id']): Promise<User['id']> {
+    // delete a existing post
+    // [TODO] remove this return to use a fetch API
+    const { data } = await axios.delete(`${base_url}/${userID}`)
+    return data
+}
+
 
 async function getUser(id: User['id']): Promise<User> {
     // Get a user
@@ -62,4 +83,4 @@ async function getUser(id: User['id']): Promise<User> {
     }
 }
 
-export { getAllUser, getUser }
+export { getAllUser, getUser,createUser,updateUser,deleteUser }
